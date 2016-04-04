20f488ba474360b35cd687d5dbe114a901b03195
artist1987 <donbass_turizm@mail.ru>
2016-04-04 13:31:36 +0300

    change

--- js/common.js
+++ js/common.js
@@ -1,15 +1,11 @@
 var CardHolder = React.createClass({
   render: function() {
    var createCard = function(card) {
-      return <input type="text" name={card.name} placeholder={card.name} key={card.id}>{card.text}</input>;
-    };
-    return <form class="pop_form">{this.props.cards.map(createCard)}</form>;
-  }
+    return <input type="text" name={card.name} placeholder={card.name} key={card.id}>{card.text}</input>;
+  };
+  return <form class="pop_form">{this.props.cards.map(createCard)}</form>;
+}
 });
-
-var List = React.createClass({
-
-});;
 
 var TrelloApp = React.createClass({
   getInitialState: function() {
@@ -22,13 +18,11 @@ var TrelloApp = React.createClass({
   },
   render: function() {
     return (
-      <div>
-        <h3>TODO</h3>
-        <CardHolder cards={this.state.cards} />
-        <a class="add" href="#" onClick={this.handleClick}><span>{'Add a card #' + (this.state.cards.length + 1)}</span></a>
+      <div>      
+      <CardHolder cards={this.state.cards} />
+      <a class="add" href="#" onClick={this.handleClick}><span>{'Add a card #' + (this.state.cards.length + 1)}</span></a>
       </div>
-    );
+      );
   }
 });
-
-ReactDOM.render(<TrelloApp />, document.getElementById('new-list'));
+ReactDOM.render(<TrelloApp />, document.getElementById('new-card'));
